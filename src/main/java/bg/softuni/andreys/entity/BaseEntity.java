85ce06175ae74ebfa8ca2fc0dffacdcb28cc39ef
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-09 16:27:08 +0200

    Spring-Fundamentals-Andreys

--- src/main/java/bg/softuni/andreys/entity/BaseEntity.java
+++ src/main/java/bg/softuni/andreys/entity/BaseEntity.java
@@ -3,8 +3,12 @@ package bg.softuni.andreys.entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
+@Setter
+@Getter
 @MappedSuperclass
 public class BaseEntity {
 
@@ -14,13 +18,5 @@ public class BaseEntity {
     private String id;
 
     protected BaseEntity() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 }
