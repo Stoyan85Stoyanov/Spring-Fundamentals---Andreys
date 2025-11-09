3e9f764190fdc4f79a13677d9649facd2eaf1183
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-09 16:27:17 +0200

    Spring-Fundamentals-Andreys

--- src/main/java/bg/softuni/andreys/entity/Category.java
+++ src/main/java/bg/softuni/andreys/entity/Category.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "categories")
 public class Category extends BaseEntity {
 
@@ -22,21 +26,5 @@ public class Category extends BaseEntity {
     public Category(CategoryEnum name, String description) {
         this.setName(name);
         this.setDescription(description);
-    }
-
-    public CategoryEnum getName() {
-        return name;
-    }
-
-    public void setName(CategoryEnum name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
