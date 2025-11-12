b54219260aff07737d8d6a018bbf3366507a040a
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-12 19:07:57 +0200

    Spring-Fundamentals-Andreys

--- src/main/java/bg/softuni/andreys/dto/UserRegisterDto.java
+++ src/main/java/bg/softuni/andreys/dto/UserRegisterDto.java
@@ -1,9 +1,11 @@
 package bg.softuni.andreys.dto;
 
 import jakarta.validation.constraints.*;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
+@Data
 public class UserRegisterDto {
 
     @NotBlank
@@ -22,47 +24,4 @@ public class UserRegisterDto {
 
     @NotBlank
     private String confirmPassword;
-
-    public UserRegisterDto() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public BigDecimal getBudget() {
-        return budget;
-    }
-
-    public void setBudget(BigDecimal budget) {
-        this.budget = budget;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
 }
