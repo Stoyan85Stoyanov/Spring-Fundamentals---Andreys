2e959f31804a16732b5b8505d61c1c63e758b814
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-12 19:07:45 +0200

    Spring-Fundamentals-Andreys

--- src/main/java/bg/softuni/andreys/entity/User.java
+++ src/main/java/bg/softuni/andreys/entity/User.java
@@ -3,10 +3,16 @@ package bg.softuni.andreys.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Setter
+@Getter
 @Entity
+@NoArgsConstructor
 @Table(name = "users")
 public class User extends BaseEntity {
 
@@ -21,38 +27,4 @@ public class User extends BaseEntity {
 
     private BigDecimal budget;
 
-    public User() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public BigDecimal getBudget() {
-        return budget;
-    }
-
-    public void setBudget(BigDecimal budget) {
-        this.budget = budget;
-    }
 }
