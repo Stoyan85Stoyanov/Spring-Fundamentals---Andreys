75768010991934266fd67687e1d0c03a6c91fe20
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-12 19:07:31 +0200

    Spring-Fundamentals-Andreys

--- src/main/java/bg/softuni/andreys/dto/ItemInfoDto.java
+++ src/main/java/bg/softuni/andreys/dto/ItemInfoDto.java
@@ -3,9 +3,17 @@ package bg.softuni.andreys.dto;
 import bg.softuni.andreys.entity.Category;
 import bg.softuni.andreys.entity.Item;
 import bg.softuni.andreys.entity.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemInfoDto {
 
     private String id;
@@ -14,61 +22,4 @@ public class ItemInfoDto {
     private BigDecimal price;
     private Category category;
     private Gender gender;
-
-    public ItemInfoDto(Item item) {
-        this.id = item.getId();
-        this.name = item.getName();
-        this.description = item.getDescription();
-        this.price = item.getPrice();
-        this.category = item.getCategory();
-        this.gender = item.getGender();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
 }
